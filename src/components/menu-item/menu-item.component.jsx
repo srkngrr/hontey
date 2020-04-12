d20f2e83afb8673c7e889f5fe354ab440525a0c0
Serkan <serkangurer@yahoo.com>
2020-04-12 21:33:21 +0300

    adding useractiontypes and Redirect component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,7 +5,7 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({title, imageUrl, size, linkUrl, history, match }) => {
 	return (
-		<div className={`${size} menu-item`} onCLick={() => history.push(`${match.url}${linkUrl}`)}>
+		<div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
 			<div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} />
 			<div className='content'>
 				<h1 className='title'>{title.toUpperCase()}</h1>
